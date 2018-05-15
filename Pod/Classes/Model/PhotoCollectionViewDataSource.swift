6c8959fe5261fabadb76e90431da778da422534b
sakdanupong <sakdanupong.v@gmail.com>
2018-05-15 12:11:22 +0700

    Set image code improvement

--- Pod/Classes/Model/PhotoCollectionViewDataSource.swift
+++ Pod/Classes/Model/PhotoCollectionViewDataSource.swift
@@ -109,7 +109,9 @@ final class PhotoCollectionViewDataSource : NSObject, UICollectionViewDataSource
         
         // Request image
         cell.requestImageId = Int(photosManager.requestImage(for: asset, targetSize: imageSize, contentMode: imageContentMode, options: nil) { (result, _) in
-            cell.imageView.image = result
+            if cell.asset?.localIdentifier == asset.localIdentifier {
+                cell.imageView.image = result
+            }
         })
         
         // Request Editing input
